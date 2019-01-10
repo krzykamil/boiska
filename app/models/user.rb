edc84dcbee79fe2a69108e45360dd0bfc8cc6ec7
LukBro <lukaszbrowar@gmail.com>
2019-01-10 14:24:18 +0100

    usuwam dupę

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+
   has_many :reservations
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, length: { is: 9 }, numericality: true
+  has_secure_password
+
 end
