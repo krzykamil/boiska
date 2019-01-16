37348e9fdacbfb0f77fcea56f750553c0d3c91c0
LukBro <lukaszbrowar@gmail.com>
2019-01-16 09:56:59 +0100

    Add field password_confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181218145404) do
+ActiveRecord::Schema.define(version: 20190116073222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20181218145404) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_confirmation"
   end
 
 end
