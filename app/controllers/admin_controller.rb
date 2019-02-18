d11a09ba41e0361a8111c8a4e07b3b7eb9d0510e
LukBro <lukaszbrowar@gmail.com>
2019-02-18 12:35:53 +0100

    faeture_add_pagination

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,4 @@
 class AdminController < ApplicationController
-  include Pagy::Backend
   before_action :authorize
   def authorize
     redirect_to new_session_path, alert: 'not authorize' if current_user.nil?
